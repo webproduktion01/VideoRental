6b64cc57b901ab3a56382918ed45bcc942f729b6
a b <anders9943@hotmail.com>
2016-04-01 09:35:42 +0200

    Initial project creation and first table designed

---
+++ VideoRental/VideoRental.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[VideoRental]
+(
+	[RentalId] INT NOT NULL PRIMARY KEY, 
+    [MovieID] INT NOT NULL, 
+    [RentDate] DATE NOT NULL, 
+    [ReturnDate] DATE NOT NULL, 
+    [RentCostPerDay] MONEY NULL, 
+    [MediaType] BIT NOT NULL
+)
